7d02ac0e999aa9bcf29438ff4a769e6f84a15e6c
Olgierd <olgierdkarpacz2000@gmail.com>
2024-03-14 02:25:52 +0100

    Added CalendarDays & improved the styling on mobile

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,36 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; 
 function Calendar() {
   const months = [
     'January', 'February', 'March', 'April', 'May', 'June',
     'July', 'August', 'September', 'October', 'November', 'December'
   ];
 
-  const currentYear = new Date().getFullYear();
+  const [selectedMonth, setSelectedMonth] = useState(null);
 
   const handleMonthClick = (monthIndex) => {
-   //Przejscie do miesiaca(pokazanie wszystkich dni)
+    setSelectedMonth(monthIndex);
   };
 
-  const renderMonthGrid = () => {
-    return (
-      <div className="calendar-grid grid grid-col-5 grid-row-3 flex gap-6 justify-center">
-        {months.map((month, index) => (
-          <button
-            key={index}
-            onClick={() => handleMonthClick(index)}
-            className="month-button p-2 flex h-100 w-90 font-bold text-white rounded items-center justify-center"
-          >
-            {month}
-          </button>
-        ))}
-      </div>
-    );
-  };
   return (
     <div>
-      {renderMonthGrid()}
+      <div className="calendar-grid grid grid-cols-5 grid-rows-3 gap-6 justify-center">
+        {months.map((month, index) => (
+          <Link
+            key={index}
+            to={`./CalendarDays/${month}`}
+            className="month-button p-2 bg-slate-800 w-40 h-40 rounded-md text-white font-bold flex items-center justify-center hover:text-teal-500 duration-300 hover:shadow-teal-600 border-2 hover:border-teal-600 border-transparent"
+          >
+            {month}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
